f3aee8f1ea5594b384137e54b03ad9af0560a4be
rioto.nagano <kingdom950927@gmail.com>
2026-07-06 22:11:44 +0900

    add: vpcモジュールの作成

--- environments/dev/variables.tf
+++ environments/dev/variables.tf
@@ -1,9 +1,12 @@
 variable "env" {
   description = "開発環境用の環境変数"
   type        = string
-  default     = "dev"
 }
 
 variable "my_ip" {
+  type = string
+}
+
+variable "vpc_cidr" {
   type = string
 }
